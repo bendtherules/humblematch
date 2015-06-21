3c72899823a4d0d6b424665dcde5d36a793d3c70
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2015-06-21 13:01:01 +0530

    Semi-finalized directory structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='humblematch',
+    packages=['humblematch'],  # this must be the same as the name above
+    version='0.1.dev1',
+    description='Will Sanitize your type-checks and duck-checks for you',
+    author='bendtherules',
+    author_email='bendtherules@codesp.in',
+    url='https://github.com/bendtherules/humblematch',  # use the URL to the github repo
+    download_url='https://github.com/bendtherules/humblematch/tarball/0.1.dev1',  # I'll explain this in a second
+    keywords=['type-checking', 'testing', 'assert'],  # arbitrary keywords
+    classifiers=['Development Status :: 3 - Alpha', ],
+)
